091939b960dd6bc12f99c891a3c45ff21c48f09d
PolTellJove <poltelljove@gmail.com>
2021-11-07 04:12:27 +0100

    Segundo commit pantalla login

--- AMS2_KahootProject/src/pack/Login.java
+++ AMS2_KahootProject/src/pack/Login.java
@@ -1,12 +1,10 @@
 package pack;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JTextPane;
 import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.SwingConstants;
@@ -14,21 +12,15 @@ import javax.swing.JTextField;
 import javax.swing.JPasswordField;
 import javax.swing.JButton;
 import javax.swing.ImageIcon;
-import javax.swing.UIManager;
 import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.SystemColor;
-import java.awt.Toolkit;
-
 import javax.swing.JCheckBox;
 
+@SuppressWarnings("serial")
 public class Login extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField textField;
 	private JPasswordField passwordField;
-	private int width = 450;
-	private int height = 300;
 
 	/**
 	 * Launch the application.
@@ -38,6 +30,7 @@ public class Login extends JFrame {
 			public void run() {
 				try {
 					Login frame = new Login();
+					frame.setLocationRelativeTo(null);
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -51,12 +44,11 @@ public class Login extends JFrame {
 	 */
 	public Login() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("KADAMM");
+		ImageIcon img = new ImageIcon("logoKadamm.png");
+		setIconImage(img.getImage());
 		setResizable(false);
-		Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
-	    int x = (int) ((dimension.getWidth() - width) / 2);
-	    int y = (int) ((dimension.getHeight() - height) / 2);
-	    System.out.println(x+" "+y);
-		setBounds(458, 234, width, height);
+		setSize(450, 300);
 		contentPane = new JPanel();
 		contentPane.setBackground(Color.decode("#374151"));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -64,38 +56,39 @@ public class Login extends JFrame {
 		contentPane.setLayout(null);
 		
 		JLabel lblUser = new JLabel("User:");
-		lblUser.setForeground(Color.WHITE);
-		lblUser.setBounds(130, 100, 80, 17);
+		lblUser.setForeground(new Color(175, 238, 238));
+		lblUser.setBounds(128, 100, 86, 17);
 		contentPane.add(lblUser);
 		
 		JLabel lblPassword = new JLabel("Password:");
-		lblPassword.setForeground(Color.WHITE);
-		lblPassword.setBounds(130, 135, 80, 17);
+		lblPassword.setForeground(new Color(175, 238, 238));
+		lblPassword.setBounds(128, 135, 86, 17);
 		contentPane.add(lblPassword);
 		
 		textField = new JTextField();
-		textField.setBounds(220, 96, 100, 20);
+		textField.setBounds(216, 96, 100, 20);
 		contentPane.add(textField);
 		textField.setColumns(10);
 		
 		passwordField = new JPasswordField();
-		passwordField.setBounds(220, 131, 100, 20);
+		passwordField.setBounds(216, 131, 100, 20);
 		contentPane.add(passwordField);
 		
 		JButton btnLogin = new JButton("Login");
+		btnLogin.setForeground(new Color(0, 0, 0));
 		btnLogin.setBounds(170, 200, 110, 35);
 		contentPane.add(btnLogin);
 		
 		JLabel lblKadamm = new JLabel("KADAMM");
-		lblKadamm.setForeground(Color.WHITE);
+		lblKadamm.setForeground(Color.CYAN);
 		lblKadamm.setHorizontalAlignment(SwingConstants.CENTER);
-		lblKadamm.setFont(new Font("Poor Richard", Font.BOLD, 52));
-		lblKadamm.setBounds(0, 20, 444, 61);
+		lblKadamm.setFont(new Font("Chiller", Font.BOLD, 70));
+		lblKadamm.setBounds(0, 25, 444, 61);
 		contentPane.add(lblKadamm);
 		
 		JCheckBox chckbxRememberMyPassword = new JCheckBox("Remember my password");
-		chckbxRememberMyPassword.setForeground(Color.WHITE);
-		chckbxRememberMyPassword.setBounds(130, 165, 180, 25);
+		chckbxRememberMyPassword.setForeground(new Color(175, 238, 238));
+		chckbxRememberMyPassword.setBounds(125, 165, 180, 25);
 		contentPane.add(chckbxRememberMyPassword);
 		chckbxRememberMyPassword.setBackground(new Color(0,0,0,0));
 		
