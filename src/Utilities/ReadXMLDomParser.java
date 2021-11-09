7a0613c5e31bcafa841cd003f0e3585810b1751c
ladyangel1981 <angiemarie1981@hotmail.com>
2021-11-09 16:31:53 +0100

    desktop app checked

---
+++ src/Utilities/ReadXMLDomParser.java
@@ -0,0 +1,71 @@
+package Utilities;
+
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class ReadXMLDomParser {
+
+	private static final String FILENAME = "src"+File.separator+"configXML"+File.separator+"config.xml";
+
+	public static void main(String[] args) {
+
+		// Instantiate the Factory
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+
+		try {
+
+			// process XML securely, avoid attacks like XML External Entities (XXE)
+			dbf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+
+			// parse XML file
+			DocumentBuilder db = dbf.newDocumentBuilder();
+
+			Document doc = db.parse(new File(FILENAME));
+
+			doc.getDocumentElement().normalize();
+
+			System.out.println("Root Element : " + doc.getDocumentElement().getNodeName());
+			System.out.println("------------------------------");
+
+			// get <staff>
+			NodeList list = doc.getElementsByTagName("Configuration");
+			Node node = list.item(0);
+			Element element = (Element) node;
+
+			// get text
+			String idioma = element.getElementsByTagName("Idioma").item(0).getTextContent();
+			NodeList tiposNodeList = element.getElementsByTagName("TipoPregunta");
+			String tipo1 = tiposNodeList.item(0).getTextContent();
+			String tipo2 = tiposNodeList.item(1).getTextContent();
+			String tipo3 = tiposNodeList.item(2).getTextContent();
+			String tipo4 = tiposNodeList.item(3).getTextContent();
+			String timeout = element.getElementsByTagName("TimeoutPreguntas").item(0).getTextContent();
+			String nummax = element.getElementsByTagName("NumMaxPreguntas").item(0).getTextContent();
+			String ruta = element.getElementsByTagName("RutaXMLKahoots").item(0).getTextContent();
+
+			// print text
+			System.out.println("Idioma : " + idioma);
+			System.out.println("Tipos de Preguntas : \n\t" + tipo1 + "\n\t" + tipo2 + "\n\t" + tipo3 + "\n\t" + tipo4);
+			System.out.println("Timeout entre Preguntas : " + timeout);
+			System.out.println("Numero Maximo de Preguntas : " + nummax);
+			System.out.println("Ruta XML de Kahoots : " + ruta);
+
+		} catch (ParserConfigurationException | SAXException | IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
--- src/views/Login.java
+++ src/views/Login.java
@@ -1,4 +1,4 @@
-package pack;
+package views;
 
 import java.awt.EventQueue;
 
@@ -7,6 +7,8 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.io.File;
+
 import javax.swing.SwingConstants;
 import javax.swing.JTextField;
 import javax.swing.JPasswordField;
@@ -45,61 +47,60 @@ public class Login extends JFrame {
 	public Login() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("KADAMM");
-		ImageIcon img = new ImageIcon("logoKadamm.png");
+		ImageIcon img = new ImageIcon("src"+File.separator+"images"+File.separator+"logoKadamm.png");
 		setIconImage(img.getImage());
 		setResizable(false);
-		setSize(450, 300);
+		setSize(466, 289);
 		contentPane = new JPanel();
 		contentPane.setBackground(Color.decode("#374151"));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
+		JLabel label_1 = new JLabel("");
+		label_1.setIcon(new ImageIcon("src"+File.separator+"images"+File.separator+"movilLoginDegr.png"));
+		label_1.setBounds(237, -50, 269, 317);
+		contentPane.add(label_1);
+		
+		JLabel label = new JLabel("");
+		label.setIcon(new ImageIcon("src"+File.separator+"images"+File.separator+"trofeoLoginDegr.png"));
+		label.setBounds(-23, -50, 260, 328);
+		contentPane.add(label);
+		
+		JLabel lblKadamm = new JLabel("KADAMM");
+		lblKadamm.setHorizontalAlignment(SwingConstants.CENTER);
+		lblKadamm.setForeground(Color.CYAN);
+		lblKadamm.setFont(new Font("Chiller", Font.BOLD, 70));
+		lblKadamm.setBounds(0, 21, 452, 61);
+		contentPane.add(lblKadamm);
+		
+		textField = new JTextField();
+		textField.setColumns(10);
+		textField.setBounds(216, 88, 100, 20);
+		contentPane.add(textField);
+		
 		JLabel lblUser = new JLabel("User:");
 		lblUser.setForeground(new Color(175, 238, 238));
-		lblUser.setBounds(128, 100, 86, 17);
+		lblUser.setBounds(128, 92, 86, 17);
 		contentPane.add(lblUser);
 		
 		JLabel lblPassword = new JLabel("Password:");
 		lblPassword.setForeground(new Color(175, 238, 238));
-		lblPassword.setBounds(128, 135, 86, 17);
+		lblPassword.setBounds(128, 127, 86, 17);
 		contentPane.add(lblPassword);
 		
-		textField = new JTextField();
-		textField.setBounds(216, 96, 100, 20);
-		contentPane.add(textField);
-		textField.setColumns(10);
-		
 		passwordField = new JPasswordField();
-		passwordField.setBounds(216, 131, 100, 20);
+		passwordField.setBounds(216, 123, 100, 20);
 		contentPane.add(passwordField);
-		
-		JButton btnLogin = new JButton("Login");
-		btnLogin.setForeground(new Color(0, 0, 0));
-		btnLogin.setBounds(170, 200, 110, 35);
-		contentPane.add(btnLogin);
-		
-		JLabel lblKadamm = new JLabel("KADAMM");
-		lblKadamm.setForeground(Color.CYAN);
-		lblKadamm.setHorizontalAlignment(SwingConstants.CENTER);
-		lblKadamm.setFont(new Font("Chiller", Font.BOLD, 70));
-		lblKadamm.setBounds(0, 25, 444, 61);
-		contentPane.add(lblKadamm);
 		
 		JCheckBox chckbxRememberMyPassword = new JCheckBox("Remember my password");
 		chckbxRememberMyPassword.setForeground(new Color(175, 238, 238));
-		chckbxRememberMyPassword.setBounds(125, 165, 180, 25);
+		chckbxRememberMyPassword.setBackground(new Color(0, 0, 0, 0));
+		chckbxRememberMyPassword.setBounds(125, 157, 180, 25);
 		contentPane.add(chckbxRememberMyPassword);
-		chckbxRememberMyPassword.setBackground(new Color(0,0,0,0));
 		
-		JLabel label = new JLabel("");
-		label.setIcon(new ImageIcon("trofeoLoginDegr.png"));
-		label.setBounds(-25, -35, 235, 350);
-		contentPane.add(label);
-		
-		JLabel label_1 = new JLabel("");
-		label_1.setIcon(new ImageIcon("movilLoginDegr.png"));
-		label_1.setBounds(240, -35, 235, 350);
-		contentPane.add(label_1);
+		JButton btnLogin = new JButton("Login");
+		btnLogin.setBounds(170, 192, 110, 35);
+		contentPane.add(btnLogin);
 	}
 }
